f331980e6eb5af148073753f60b8dfc745a38229
Ully Machado <103787911+UllyMachado@users.noreply.github.com>
2022-07-09 17:24:50 -0300

    Room e funcionalidades de "Share" e "Favorite"

--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,6 +1,5 @@
 package me.dio.soccernews.ui.news;
 
-import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -17,37 +16,50 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
+    public enum State {
+        DOING, DONE, ERROR;
+    }
+
     private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final MutableLiveData<State> state = new MutableLiveData<>();
     private final SoccerNewsApi api;
 
     public NewsViewModel() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://ullymachado.github.io/soccer-news-api/")
+                .baseUrl("https://digitalinnovationone.github.io/soccer-news-api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         api = retrofit.create(SoccerNewsApi.class);
+
         this.findNews();
     }
 
     private void findNews() {
+        state.setValue(State.DOING);
         api.getNews().enqueue(new Callback<List<News>>() {
             @Override
-            public void onResponse(@NonNull Call<List<News>> call, @NonNull Response<List<News>> response) {
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
                 if (response.isSuccessful()) {
                     news.setValue(response.body());
+                    state.setValue(State.DONE);
                 } else {
-                    //TODO Pensar em uma estratégia de tratamento de erros.
+                    state.setValue(State.ERROR);
                 }
             }
 
             @Override
-            public void onFailure(@NonNull Call<List<News>> call, @NonNull Throwable t) {
-                //TODO Pensar em uma estratégia de tratamento de erros.
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                t.printStackTrace();
+                state.setValue(State.ERROR);
             }
         });
     }
 
     public LiveData<List<News>> getNews() {
         return this.news;
+    }
+
+    public LiveData<State> getState() {
+        return this.state;
     }
 }
